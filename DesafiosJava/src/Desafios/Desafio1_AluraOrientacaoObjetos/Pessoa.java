85316fd2e487ca3236ac4748191a751abb4a5dfb
Gabriel <116319547+Salkaiju@users.noreply.github.com>
2025-08-06 10:42:41 -0300

    Delete DesafiosJava directory

--- DesafiosJava/src/Desafios/Desafio1_AluraOrientacaoObjetos/Pessoa.java
+++
@@ -1,8 +0,0 @@
-package Desafios.Desafio1_AluraOrientacaoObjetos;
-
-public class Pessoa {
-    public static void OlaMundo() {
-        System.out.println("Olá mundo");
-    } //fim "Função Main"
-
-} //fpc
